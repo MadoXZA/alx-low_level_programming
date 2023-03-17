cc5b0ad5a265ef85f3a1a37c594d6d047e7d5776
MadoXZA <timklo15@gmail.com>
2023-03-17 05:05:12 -0700

    9 fixed

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,14 +10,13 @@ int main(void)
 
 	for (n = 48; n <= 57; n++)
 	{
-		putchar(n + 48);
+		putchar(n);
 		if (n == 57)
 		{
 			continue;
 		}
 		putchar(',');
 		putchar(' ');
-	
 	}
 	putchar('\n');
 	return (0);
