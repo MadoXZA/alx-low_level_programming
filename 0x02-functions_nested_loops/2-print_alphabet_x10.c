e72fdd64e3c8d2fbd87fe768d2a99f52caf36783
MadoXZA <timklo15@gmail.com>
2023-03-21 00:06:56 -0700

    corrections

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,18 +4,18 @@
  *
  * Return: Always 0 (success)
  */
+
 void print_alphabet_x10(void)
 {
 	int i;
 	int j;
-	
+
 	for (i = 1; i <= 10; i++)
 	{
-		for j = 97; j <= 122; j++)
+		for ((j = 97); j <= 122; j++);
 		{
 			_putchar(j);
 		}
 		_putchar('\n');
 	}
-	return (0);
 }
