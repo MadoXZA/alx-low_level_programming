db4530f752a17b77eb21df28bdc8d9a3e143b188
MadoXZA <timklo15@gmail.com>
2023-03-21 07:47:52 -0700

    fixed

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,9 +9,9 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			k = j * i;
 			if (j == 0)
@@ -30,8 +30,8 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
+				_putchar((k / 9) + '0');
+				_putchar((k % 9) + '0');
 			}
 		}
 		_putchar('\n');
