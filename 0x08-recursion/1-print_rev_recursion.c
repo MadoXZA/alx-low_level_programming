ad5de643012ae96fd08e3005200cb3e5ab3ed788
MadoXZA <timklo15@gmail.com>
2023-04-05 02:06:42 -0700

    Task 2 and TAsk 3 complete

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include"main.h"
 
 /**
- * _print_rev_recusrion - prints a string in reverse
+ * _print_rev_recursion - prints a string in reverse
  * @s: The string to be printed
  */
 
@@ -9,7 +9,7 @@ void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		_print_rev_recursion(s+1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
