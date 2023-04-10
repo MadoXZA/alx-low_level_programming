2560db0d4165eedbc03cff8d71109738cdc3bde2
MadoXZA <timklo15@gmail.com>
2023-04-10 07:22:59 -0700

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,9 @@
  * @argv: the array of arguments
  */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
+	(void)argc;
 	printf("%s\n", argv[0]);
 	return 0;
 }
