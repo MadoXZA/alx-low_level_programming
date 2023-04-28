519ac6ed4a2ba7200e97cdb66a1770abde537890
MadoXZA <timklo15@gmail.com>
2023-04-28 06:05:40 -0700

    Task 2 correction

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,26 +3,34 @@
 #include "lists.h"
 
 /**
- * add_node - adds node 
+ * add_node - adds node
  * @head: double pointer
  * @str: string to add in node
  *
- * Return: address or NULL if fails
+ * Return: address of element or NULL if fails
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
+	size_t len;
 	list_t *nuut;
-	unsigned int len = 0;
 
-	while (str[len])
-		len++;
-
-	nuut = malloc(sizeof(list_t));
-	if (!nuut)
+	if (str == NULL)
 		return (NULL);
 
-	nuut->str = strdup(str);
+	len = strlen(str);
+	nuut = (list_t *) malloc(sizeof(list_t));
+	if (nuut == NULL)
+		return (NULL);
+	nuut->str = (char *) malloc((len + 1) * sizeof(char));
+	if (nuut->str == NULL)
+	{
+		free(nuut);
+		return (NULL);
+	}
+
+
+	strcpy(nuut->str, str);
 	nuut->len = len;
 	nuut->next = (*head);
 	(*head) = nuut;
