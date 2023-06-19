f5eb1ddd6dd55a93b0d84c5a7c876fed2d229d53
MadoXZA <timklo15@gmail.com>
2023-06-19 07:31:22 -0700

    Completed Task

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * _islower - Shows 1 if the output is a lowercase, Another case, shows 0
+ *
+ * @c: the character in ASCII code
+ *
+ * Return: 1 for lowercase character, 0 otherwise.
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+
+}
